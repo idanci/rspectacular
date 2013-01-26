51a9063a6f03a5e936fe70aa742a0a3697a5d700
Jeff Felchner <accounts+git@thekompanee.com>
2013-01-25 23:42:55 -0600

    Set up an 'as fast as possible' database cleaner configuration

--- lib/rspectacular/plugins/database_cleaner.rb
+++ lib/rspectacular/plugins/database_cleaner.rb
@@ -1,9 +1,17 @@
-if defined? DatabaseCleaner
+if defined? DatabaseCleaner && defined? RSpec::Rails
   RSpec.configure do |config|
     config.use_transactional_fixtures = false
 
     config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    config.before(:each, :js => true) do
+      DatabaseCleaner.strategy = :truncation, { :pre_count => true }
     end
 
     config.before(:each) do
@@ -13,5 +21,9 @@ if defined? DatabaseCleaner
     config.after(:each) do
       DatabaseCleaner.clean
     end
+  end
+else
+  RSpec.configure do |config|
+    config.use_transactional_fixtures = true
   end
 end
