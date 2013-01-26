8054fccc2f8542da9aaf4998ed94b7a90539c7b2
Jeff Felchner <accounts+git@thekompanee.com>
2013-01-25 23:43:07 -0600

    Second pass at default selectors

--- lib/rspectacular/selectors/defaults.rb
+++ lib/rspectacular/selectors/defaults.rb
@@ -1,47 +1,50 @@
-RSpectacular::Selectors ||= {}
-RSpectacular::Selectors.merge(
-  /the Facebook application/ => lambda do
-    frame_element = find 'html#facebook div#pagelet_app_runner iframe'
-    frame_element[:id]
-  end,
+module RSpectacular
+  Selectors ||= {}
 
-  /the flash(.*)/ => lambda do
-    flash_type_class = $1.strip
-    flash_type_class = flash_type_class.empty? ? '' : ".#{flash_type_class}"
+  Selectors.merge!(
+    /the Facebook application/ => lambda do
+      frame_element = find 'html#facebook div#pagelet_app_runner iframe'
+      frame_element[:id]
+    end,
 
-    ".flash#{flash_type_class} p"
-  end,
+    /the flash(.*)/ => lambda do
+      flash_type_class = $1.strip
+      flash_type_class = flash_type_class.empty? ? '' : ".#{flash_type_class}"
 
-  ###
-  # Facebook
-  #
-  /the Facebook login form/             => 'html#facebook form#login_form',
-  /the Facebook page timeline nav bar/  => 'html#facebook #fbTimelineNavTopRow',
-  /the Facebook account menu/           => '#navAccountLink',
+      ".flash#{flash_type_class} p"
+    end,
 
-  ###
-  # PayPal
-  #
-  /the "Pay with PayPal" button/        => 'input[alt="Check out with PayPal"]',
+    ###
+    # Facebook
+    #
+    /the Facebook login form/             => 'html#facebook form#login_form',
+    /the Facebook page timeline nav bar/  => 'html#facebook #fbTimelineNavTopRow',
+    /the Facebook account menu/           => '#navAccountLink',
 
-  ###
-  # Forms
-  #
-  /the errors for (.*)/                 => "#{sf $1}+div.error",
+    ###
+    # PayPal
+    #
+    /the "Pay with PayPal" button/        => 'input[alt="Check out with PayPal"]',
 
-  ###
-  # Windows
-  #
-  /the most recently opened window/     => page.driver.browser.window_handles.last,
-  /the alert dialog/                    => page.driver.browser.switch_to.alert,
+    ###
+    # Forms
+    #
+    /the errors for (.*)/                 => -> { "#{sf $1}+div.error" },
 
-  ###
-  # Date Picker Buttons
-  #
-  /the date picker button for today/    => '.ui-datepicker-today',
+    ###
+    # Windows
+    #
+    /the most recently opened window/     => -> { page.driver.browser.window_handles.last },
+    /the alert dialog/                    => -> { page.driver.browser.switch_to.alert },
 
-  ###
-  # Model Links
-  #
-  /the (.*) button for/                 => "##{$1.gsub(/ /, '_')}_#{args[0].class.name.underscore}_#{args[0].id}_link"
-)
+    ###
+    # Date Picker Buttons
+    #
+    /the date picker button for today/    => '.ui-datepicker-today',
+
+    ###
+    # Model Links
+    #
+    /the (.*) button for/                 => -> { "##{$1.gsub(/ /, '_')}_#{args[0].class.name.underscore}_#{args[0].id}_link" }
+  )
+end
