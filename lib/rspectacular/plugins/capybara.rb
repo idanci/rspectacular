066be100f8ece1b2cb15eeddb8fe2e38a3bb0209
Jeff Felchner <accounts+git@thekompanee.com>
2013-01-25 23:42:31 -0600

    Remove the invalid Capybara config and instead set the default Javascript driver to :webkit if capybara-webkit is installed

--- lib/rspectacular/plugins/capybara.rb
+++ lib/rspectacular/plugins/capybara.rb
@@ -1,5 +1,3 @@
-Capybara.javascript_driver = :selenium
-
-Capybara.default_host = Chirrpy::CONFIG[:host_with_port]
-Capybara.app_host     = Chirrpy::CONFIG[:base_url]
-Capybara.server_port  = Chirrpy::CONFIG[:port]
+if defined? Capybara::Webkit
+  Capybara.javascript_driver = :webkit
+end
