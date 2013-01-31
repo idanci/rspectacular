e20dd9b5e3a6140a927d492de1f8f40091b453d1
Jeff Felchner <accounts+git@thekompanee.com>
2013-01-30 23:00:58 -0600

    Move the color configuration into the formatters file because we want it to work even if active_record_spec_helper is not required but rspectacular is

--- lib/rspectacular/spec_helpers/active_record_spec_helper.rb
+++ lib/rspectacular/spec_helpers/active_record_spec_helper.rb
@@ -19,5 +19,4 @@ Dir[File.expand_path('../support/**/*.rb',   __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.order = 'random'
-  config.color = true
 end
--- lib/rspectacular/support/formatters.rb
+++ lib/rspectacular/support/formatters.rb
@@ -1,4 +1,6 @@
 RSpec.configure do |config|
+  config.color = true
+
   config.add_formatter 'Fuubar'
   config.add_formatter 'documentation', 'tmp/rspec.txt'
 end
