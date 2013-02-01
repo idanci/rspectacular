3b8091bd7d909a84c018ca710e49e78a6e6bd0b2
Jeff Felchner <accounts+git@thekompanee.com>
2013-01-31 18:58:11 -0600

    Whitespace

--- lib/rspectacular/plugins/database_cleaner.rb
+++ lib/rspectacular/plugins/database_cleaner.rb
@@ -1,39 +1,39 @@
 if defined? ActiveRecord::Base
-begin
-  require 'database_cleaner'
+  begin
+    require 'database_cleaner'
 
-  RSpec.configure do |config|
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.strategy = :transaction
-    end
-
-    config.before(:each, :js => true) do
-      DatabaseCleaner.strategy = :truncation, { :pre_count => true }
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
-  end
-
-  if defined? RSpec::Rails
     RSpec.configure do |config|
-      config.use_transactional_fixtures = false
+      config.before(:suite) do
+        DatabaseCleaner.clean_with(:truncation)
+      end
+
+      config.before(:each) do
+        DatabaseCleaner.strategy = :transaction
+      end
+
+      config.before(:each, :js => true) do
+        DatabaseCleaner.strategy = :truncation, { :pre_count => true }
+      end
+
+      config.before(:each) do
+        DatabaseCleaner.start
+      end
+
+      config.after(:each) do
+        DatabaseCleaner.clean
+      end
+    end
+
+    if defined? RSpec::Rails
+      RSpec.configure do |config|
+        config.use_transactional_fixtures = false
+      end
+    end
+  rescue LoadError
+    if defined? RSpec::Rails
+      RSpec.configure do |config|
+        config.use_transactional_fixtures = true
+      end
     end
   end
-rescue LoadError
-  if defined? RSpec::Rails
-    RSpec.configure do |config|
-      config.use_transactional_fixtures = true
-    end
-  end
-end
 end
