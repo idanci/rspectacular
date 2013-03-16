295d1b0ab2d200d9082510126880d641b293b379
Jeff Felchner <accounts+git@thekompanee.com>
2013-03-16 11:40:26 -0500

    Require Timecop if necessary

--- lib/rspectacular/plugins/timecop.rb
+++ lib/rspectacular/plugins/timecop.rb
@@ -1,7 +1,9 @@
-if defined? Timecop
-  RSpec.configure do |config|
-    config.after(:each, :time_mock => true) do
-      Timecop.return
-    end
+RSpec.configure do |config|
+  config.before(:suite, :time_mock => true) do
+    require 'timecop'
+  end
+
+  config.after(:each, :time_mock => true) do
+    Timecop.return
   end
 end
