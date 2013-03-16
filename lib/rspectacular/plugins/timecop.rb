c9bc84736f71674d97e6cc2fa66a16d2d8e2c6f9
Jeff Felchner <accounts+git@thekompanee.com>
2013-03-16 14:47:13 -0500

    Fix Timecop mocking since :suite is not available in configuration filter

--- lib/rspectacular/plugins/timecop.rb
+++ lib/rspectacular/plugins/timecop.rb
@@ -1,6 +1,8 @@
 RSpec.configure do |config|
-  config.before(:suite, :time_mock => true) do
+  config.before(:all, :time_mock => true) do
     require 'timecop'
+
+    Timecop.freeze(Time.utc(2012, 7, 26, 18, 0, 0))
   end
 
   config.after(:each, :time_mock => true) do
