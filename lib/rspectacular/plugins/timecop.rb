08aace2fd567d06c2789b53d975595cd451ef600
Jeff Felchner <accounts+git@thekompanee.com>
2013-03-16 15:56:20 -0500

    More fixes for the Timecop plugin

--- lib/rspectacular/plugins/timecop.rb
+++ lib/rspectacular/plugins/timecop.rb
@@ -1,11 +1,14 @@
-RSpec.configure do |config|
-  config.before(:all, :time_mock => true) do
-    require 'timecop'
+begin
+  require 'timecop'
 
-    Timecop.freeze(Time.utc(2012, 7, 26, 18, 0, 0))
-  end
+  RSpec.configure do |config|
+    config.around(:each, :time_mock => true) do |example|
+      Timecop.freeze(Time.utc(2012, 7, 26, 18, 0, 0))
 
-  config.after(:each, :time_mock => true) do
-    Timecop.return
+      example.run
+
+      Timecop.return
+    end
   end
+rescue LoadError
 end
