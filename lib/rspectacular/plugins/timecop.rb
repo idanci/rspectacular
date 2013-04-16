1e0b636b866a67f86223af39dac4f9b77886293c
Jeff Felchner <accounts+git@thekompanee.com>
2013-04-16 11:51:38 -0500

    Allow the Timecop plugin to take options in its metadata
    Now you can call an example like so:
    
      it 'mocks time', :time_mock => Time.utc(2012, 9, 10, 12, 54, 33) do
        # test
      end
    
    Or by:
    
      it 'mocks time by traveling with a default time', :time_mock => { :type => :travel } do
        # test
      end
    
    OR by:
    
      it 'mocks time by freezing to "now"', :time_mock => { :type => :freeze, :time => Time.now } do
        # test
      end

--- lib/rspectacular/plugins/timecop.rb
+++ lib/rspectacular/plugins/timecop.rb
@@ -2,8 +2,13 @@ begin
   require 'timecop'
 
   RSpec.configure do |config|
-    config.around(:each, :time_mock => true) do |example|
-      Timecop.freeze(Time.utc(2012, 7, 26, 18, 0, 0))
+    config.around(:each, :time_mock => lambda { |v| !!v }) do |example|
+      options   = example.metadata[:time_mock]
+      options   = options.is_a?(Hash) ? options : {}
+      time      = options.is_a?(Time) ? options : (options[:time] || Time.utc(2012, 7, 26, 18, 0, 0))
+      mock_type = options.fetch(:type, :freeze)
+
+      Timecop.send(mock_type, time)
 
       example.run
 
