c7bfd3c0ef991e58c4d77c6acece6277a90110ae
Jeff Felchner <accounts+git@thekompanee.com>
2013-04-23 15:52:18 -0500

    Fix time mocking metadata parsing for the Timecop plugin

--- lib/rspectacular/plugins/timecop.rb
+++ lib/rspectacular/plugins/timecop.rb
@@ -4,9 +4,15 @@ begin
   RSpec.configure do |config|
     config.around(:each, :time_mock => lambda { |v| !!v }) do |example|
       options   = example.metadata[:time_mock]
-      options   = options.is_a?(Hash) ? options : {}
-      time      = options.is_a?(Time) ? options : (options[:time] || Time.utc(2012, 7, 26, 18, 0, 0))
+      options   = case options
+                  when Time
+                    { :time => options }
+                  when TrueClass
+                    {}
+                  end
+
       mock_type = options.fetch(:type, :freeze)
+      time      = options.fetch(:time, Time.utc(2012, 7, 26, 18, 0, 0))
 
       Timecop.send(mock_type, time)
 
