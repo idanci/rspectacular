9cde9af23dc8dbc812f12aeffed10c71b2bbf224
Jeff Felchner <accounts+git@thekompanee.com>
2013-04-23 14:22:23 -0500

    Only run the PayPal plugin hooks if the spec is also being run as a JS test

--- lib/rspectacular/plugins/paypal.rb
+++ lib/rspectacular/plugins/paypal.rb
@@ -3,11 +3,11 @@
 # to test users.
 #
 RSpec.configure do |config|
-  config.before(:each, :paypal => true) do
+  config.before(:each, :js => true, :paypal => true) do
     log_in_to_paypal_sandbox
   end
 
-  config.after(:each, :paypal => true) do
+  config.after(:each, :js => true, :paypal => true) do
     log_out_of_paypal
     log_out_of_paypal_sandbox
   end
