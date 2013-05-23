bad0dac2e6a77a7dd64c1eec854d2fab4547124c
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:24:07 -0500

    Convert Singleton plugin to a before call so that all Singletons are guaranteed reset before the tagged spec

--- lib/rspectacular/plugins/singleton.rb
+++ lib/rspectacular/plugins/singleton.rb
@@ -3,11 +3,12 @@
 ##############################################################################
 
 RSpec.configure do |config|
-  config.around(:each, :singletons => lambda { |v| !!v }) do |example|
-    options             = example.metadata[:singletons]
-    singletons_to_reset = options.respond_to?(:each) ? options : [options]
+  config.before(:each, :singletons => lambda { |v| !!v }) do |example|
+    require 'singleton'
 
-    example.run
+    options             = example.metadata[:singletons]
+    options             = options.is_a?(TrueClass) ? subject.class : options
+    singletons_to_reset = options.respond_to?(:each) ? options : [options]
 
     singletons_to_reset.each do |singleton|
       Singleton.__init__(singleton)
