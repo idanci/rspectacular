70e289f82b75d00b366d762b06adf0bad85031d2
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:18:23 -0500

    Whitespace

--- lib/rspectacular/plugins/database_cleaner.rb
+++ lib/rspectacular/plugins/database_cleaner.rb
@@ -2,40 +2,40 @@
 #                         Database Cleaner Plugin
 ##############################################################################
 
-  begin
-    require 'database_cleaner'
+begin
+  require 'database_cleaner'
 
+  RSpec.configure do |config|
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    config.before(:each, :js => true) do
+      DatabaseCleaner.strategy = :truncation, { :pre_count => true }
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
+
+  if defined? RSpec::Rails
     RSpec.configure do |config|
-      config.before(:suite) do
-        DatabaseCleaner.clean_with(:truncation)
-      end
-
-      config.before(:each) do
-        DatabaseCleaner.strategy = :transaction
-      end
-
-      config.before(:each, :js => true) do
-        DatabaseCleaner.strategy = :truncation, { :pre_count => true }
-      end
-
-      config.before(:each) do
-        DatabaseCleaner.start
-      end
-
-      config.after(:each) do
-        DatabaseCleaner.clean
-      end
+      config.use_transactional_fixtures = false
     end
-
-    if defined? RSpec::Rails
-      RSpec.configure do |config|
-        config.use_transactional_fixtures = false
-      end
+  end
+rescue LoadError
+  if defined? RSpec::Rails
+    RSpec.configure do |config|
+      config.use_transactional_fixtures = true
     end
-  rescue LoadError
-    if defined? RSpec::Rails
-      RSpec.configure do |config|
-        config.use_transactional_fixtures = true
-      end
-    end
+  end
 end
--- lib/rspectacular/plugins/shoulda.rb
+++ lib/rspectacular/plugins/shoulda.rb
@@ -6,19 +6,19 @@ if defined? RSpec::Rails
   begin
     require 'shoulda-matchers'
 
-  module ShouldaRoutingMatchers
-    def route(method, path)
-      Shoulda::Matchers::ActionController::RouteMatcher.new(method, path, self)
-    end
-  end
-
-  module RSpec
-    module Rails
-      module RoutingExampleGroup
-        include ShouldaRoutingMatchers
+    module ShouldaRoutingMatchers
+      def route(method, path)
+        Shoulda::Matchers::ActionController::RouteMatcher.new(method, path, self)
       end
     end
-  end
+
+    module RSpec
+      module Rails
+        module RoutingExampleGroup
+          include ShouldaRoutingMatchers
+        end
+      end
+    end
   rescue LoadError
   end
 end
