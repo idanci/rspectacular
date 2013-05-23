a67107ab896ce47bbd459bca20013a3ec36fd117
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:12:07 -0500

    Convert support file requires calls to a loop

--- lib/rspectacular/support.rb
+++ lib/rspectacular/support.rb
@@ -1,7 +1,1 @@
-require 'rspectacular/support/general'
-require 'rspectacular/support/garbage_collection'
-require 'rspectacular/support/random'
-require 'rspectacular/support/focused'
-require 'rspectacular/support/pending'
-require 'rspectacular/support/selenium'
-require 'rspectacular/support/formatters'
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
