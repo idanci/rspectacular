e18df5801df354288cabee64d2cc62c4447d0e55
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:31:28 -0500

    Whitespace

--- lib/rspectacular/plugins/database_cleaner.rb
+++ lib/rspectacular/plugins/database_cleaner.rb
@@ -12,33 +12,33 @@ begin
   end
 
   if autodetected
-  RSpec.configure do |config|
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.strategy = :transaction
-    end
-
-    config.before(:each, :js => true) do
-      DatabaseCleaner.strategy = :truncation, { :pre_count => true }
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
-  end
-
-  if defined? RSpec::Rails
     RSpec.configure do |config|
-      config.use_transactional_fixtures = false
+      config.before(:suite) do
+        DatabaseCleaner.clean_with(:truncation)
+      end
+
+      config.before(:each) do
+        DatabaseCleaner.strategy = :transaction
+      end
+
+      config.before(:each, :js => true) do
+        DatabaseCleaner.strategy = :truncation, { :pre_count => true }
+      end
+
+      config.before(:each) do
+        DatabaseCleaner.start
+      end
+
+      config.after(:each) do
+        DatabaseCleaner.clean
+      end
     end
-  end
+
+    if defined? RSpec::Rails
+      RSpec.configure do |config|
+        config.use_transactional_fixtures = false
+      end
+    end
   end
 rescue LoadError, DatabaseCleaner::NoORMDetected
   if defined? RSpec::Rails
