b6261d2ee5845ed0bbb421059b299d9d9c5a6242
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:13:43 -0500

    When using VCR, hook into webmock by default, not Faraday

--- lib/rspectacular/plugins/vcr.rb
+++ lib/rspectacular/plugins/vcr.rb
@@ -1,10 +1,11 @@
 begin
   require 'vcr'
+  require 'rspectacular/plugins/webmock'
 
   VCR.configure do |config|
     config.cassette_library_dir = 'tmp/vcr_cassettes'
 
-    config.hook_into :faraday
+    config.hook_into :webmock
     config.configure_rspec_metadata!
   end
 
