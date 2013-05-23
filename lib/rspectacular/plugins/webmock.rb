954b0fee975e0bba14cb8d9f447f44142b7db873
Jeff Felchner <accounts+git@thekompanee.com>
2013-05-22 23:13:13 -0500

    When Webmock is enabled, allow localhost by default

--- lib/rspectacular/plugins/webmock.rb
+++ lib/rspectacular/plugins/webmock.rb
@@ -1,10 +1,17 @@
+##############################################################################
+#                              WebMock Plugin
+##############################################################################
+
 begin
   require 'webmock/rspec'
 
   RSpec.configure do |config|
     config.around(:each, :web_mock => lambda { |v| !!v }) do |example|
       options = example.metadata[:web_mock]
-      options = options.is_a?(Hash) ? options : {}
+      options = case options
+                when TrueClass
+                  { :allow_localhost => true }
+                end
 
       WebMock.disable_net_connect!(options)
 
