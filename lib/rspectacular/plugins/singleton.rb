eb37759cee8d653bd2e6de2c7e1a8ad5aa502f46
Jeff Felchner <accounts+git@thekompanee.com>
2013-08-21 21:28:59 -0500

    Reset singletons both before and after an example to solve some race conditions and order issues

--- lib/rspectacular/plugins/singleton.rb
+++ lib/rspectacular/plugins/singleton.rb
@@ -3,12 +3,18 @@
 ##############################################################################
 
 RSpec.configure do |config|
-  config.before(:each, :singletons => lambda { |v| !!v }) do |example|
+  config.around(:each, :singletons => lambda { |v| !!v }) do |example|
     require 'singleton'
 
     options             = example.metadata[:singletons]
     options             = options.is_a?(TrueClass) ? subject.class : options
     singletons_to_reset = options.respond_to?(:each) ? options : [options]
+
+    singletons_to_reset.each do |singleton|
+      Singleton.__init__(singleton)
+    end
+
+    example.run
 
     singletons_to_reset.each do |singleton|
       Singleton.__init__(singleton)
