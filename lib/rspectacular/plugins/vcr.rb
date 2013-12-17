6bed0a8bc23639c155f86b0cb97a60604bdbb6cc
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-16 18:39:19 -0600

    Remove RSpec VCR macros since they are deprecated

--- lib/rspectacular/plugins/vcr.rb
+++ lib/rspectacular/plugins/vcr.rb
@@ -11,10 +11,5 @@ begin
     config.hook_into :faraday
     config.configure_rspec_metadata!
   end
-
-  RSpec.configure do |config|
-    config.extend VCR::RSpec::Macros
-  end
-
 rescue LoadError
 end
