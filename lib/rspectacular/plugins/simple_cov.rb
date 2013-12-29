cd354fb1e0c7abf3971e41aa2d1111b526e1b59a
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-29 14:40:01 -0600

    Fix SimpleCov plugin so that it ignores test and configuration directories

--- lib/rspectacular/plugins/simple_cov.rb
+++ lib/rspectacular/plugins/simple_cov.rb
@@ -1,6 +1,11 @@
 begin
   require 'simplecov'
 
-  SimpleCov.start
+  SimpleCov.start do
+    add_filter '/config/'
+    add_filter '/spec/'
+    add_filter '/test/'
+    add_filter '/features/'
+  end
 rescue LoadError
 end
