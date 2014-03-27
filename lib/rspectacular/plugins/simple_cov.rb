e720a02c9b9512ce431613b75ff8444fde6f6ffe
Jeff Felchner <accounts+git@thekompanee.com>
2014-03-27 04:33:43 -0500

    Only try to load SimpleCov if we're using Ruby > 1.8

--- lib/rspectacular/plugins/simple_cov.rb
+++ lib/rspectacular/plugins/simple_cov.rb
@@ -1,11 +1,13 @@
 begin
-  require 'simplecov'
+  unless RUBY_VERSION.match /\A1\.8/
+    require 'simplecov'
 
-  SimpleCov.start do
-    add_filter '/config/'
-    add_filter '/spec/'
-    add_filter '/test/'
-    add_filter '/features/'
+    SimpleCov.start do
+      add_filter '/config/'
+      add_filter '/spec/'
+      add_filter '/test/'
+      add_filter '/features/'
+    end
   end
 rescue LoadError
 end
