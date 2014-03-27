b9a8d3597c1275b1b6dd9d6a757cd0bcbe0124a6
Jeff Felchner <accounts+git@thekompanee.com>
2014-03-27 03:40:03 -0500

    Explode plugin list and move doc plugins to the top of the heap

--- lib/rspectacular.rb
+++ lib/rspectacular.rb
@@ -1,3 +1,7 @@
+# Coverage Plugins Must Be Loaded Before Anything Else
+require 'rspectacular/plugins/code_climate.rb'
+require 'rspectacular/plugins/simple_cov.rb'
+
 require 'rspectacular/support'
 require 'rspectacular/helpers'
 require 'rspectacular/selectors'
