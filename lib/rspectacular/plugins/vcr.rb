cd4fe193e2497f3bc38e01cfea505fe042bfb52f
Jeff Felchner <accounts+git@thekompanee.com>
2014-03-27 18:11:50 -0500

    Make VCR plugin smarter about what adapter it hooks into

--- lib/rspectacular/plugins/vcr.rb
+++ lib/rspectacular/plugins/vcr.rb
@@ -4,12 +4,28 @@
 
 begin
   require 'vcr'
-  require 'faraday'
+
+  begin
+    require 'webmock'
+
+    VCR.configure do |config|
+      config.hook_into :webmock
+    end
+  rescue LoadError
+  end
+
+  begin
+    require 'faraday'
+
+    VCR.configure do |config|
+      config.hook_into :faraday
+    end
+  rescue LoadError
+  end
 
   VCR.configure do |config|
     config.cassette_library_dir = File.expand_path('./tmp/vcr_cassettes')
 
-    config.hook_into :faraday
     config.configure_rspec_metadata!
   end
 rescue LoadError
