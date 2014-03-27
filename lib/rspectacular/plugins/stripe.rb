f809106e067b9538a6c846c0814acee70c2b4a66
Jeff Felchner <accounts+git@thekompanee.com>
2014-03-26 23:49:40 -0500

    Add a Stripe plugin

---
+++ lib/rspectacular/plugins/stripe.rb
@@ -0,0 +1,20 @@
+##############################################################################
+#                               Stripe Plugin
+##############################################################################
+
+begin
+  require 'stripe'
+
+  RSpec.configure do |config|
+    config.after(:each, :stripe) do
+      Stripe::Plan.all(count: 100).each do |plan|
+        if plan.id.match(/test/i) || plan.name.match(/test/i)
+          plan.delete
+        end
+      end
+
+      Stripe::Customer.all(count: 100).each(&:delete)
+    end
+  end
+rescue LoadError
+end
