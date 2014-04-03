e0dcd2c0534fe1f7529a68c225c3612a45fa9bec
Jeff Felchner, Blake Cash and Drew Montgomery <pair+blakecash-hhd418-jfelchner@thekompanee.com>
2014-04-03 13:54:40 -0500

    Add authenticate_api_as which sets the HTTP_AUTHORIZATION header using the user's token

--- lib/rspectacular/helpers/session_helpers.rb
+++ lib/rspectacular/helpers/session_helpers.rb
@@ -8,3 +8,9 @@ if defined? Devise
     visit destroy_user_session_path
   end
 end
+
+def authenticate_api_as(user)
+  auth_token = user.respond_to?(:auth_token) ? user.auth_token : user
+
+  request.env['HTTP_AUTHORIZATION'] = "Token #{auth_token}"
+end
