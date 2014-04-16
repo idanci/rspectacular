d003587ff7e5c585e21f6e5fe59b16008875fdb4
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-16 00:41:02 -0500

    Always load some of the helpers, even if Capybara isn't loaded

--- lib/rspectacular/helpers.rb
+++ lib/rspectacular/helpers.rb
@@ -1,3 +1,8 @@
+require 'rspectacular/helpers/factories'
+require 'rspectacular/helpers/null_objects'
+require 'rspectacular/helpers/rails_flashes'
+require 'rspectacular/helpers/session_helpers'
+
 if defined?(Capybara::Driver::Base)
   Dir[File.expand_path('../helpers/**/*.rb', __FILE__)].each { |f| require f }
 end
