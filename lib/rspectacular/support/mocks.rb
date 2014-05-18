0dbd9c75b390c6c984ea484e7d8b41d90aacd3b1
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-17 23:11:18 -0500

    Feature: Only mock using the expect syntax

--- lib/rspectacular/support/mocks.rb
+++ lib/rspectacular/support/mocks.rb
@@ -2,5 +2,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles        = true
     mocks.verify_doubled_constant_names = true
+    mocks.syntax                        = :expect
   end
 end
